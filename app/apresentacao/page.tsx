6b0ca0ac775ddd2570e000d5a93525049c3a946a
raugustorubens-design <raugustorubens@gmail.com>
2026-02-07 12:46:39 -0300

    Update page.tsx

--- app/apresentacao/page.tsx
+++ app/apresentacao/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ color: "white", padding: 40 }}>
-      <h1>Projeto Renascer — OK</h1>
-      <p>A rota raiz está funcionando.</p>
+      <h1>Rota raiz OK</h1>
+      <p>O App Router está funcionando.</p>
     </main>
   );
 }
